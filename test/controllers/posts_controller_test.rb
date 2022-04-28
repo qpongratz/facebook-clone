134742427f97abdbc60572bf0c6adfa52e432a39
Quentin Pongratz <qpongratz@gmail.com>
2022-04-28 08:47:45 -0500

    Change tests to work with devise

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @post = posts(:one)
+    @user = users(:mary)
+    sign_in @user
   end
 
   test "should get index" do
