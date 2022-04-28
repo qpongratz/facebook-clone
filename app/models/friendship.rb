39a6f62e70341ddfd27136ede37900fd14a7f1bf
Quentin Pongratz <qpongratz@gmail.com>
2022-04-28 18:52:32 -0500

    Add status to friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ApplicationRecord
+  enum :status, %i[pending accepted]
   belongs_to :requester, class_name: 'User'
   belongs_to :friend, class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_195256) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_225123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_195256) do
     t.bigint "friend_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["requester_id", "friend_id"], name: "index_friendships_on_requester_id_and_friend_id", unique: true
     t.index ["requester_id"], name: "index_friendships_on_requester_id"
