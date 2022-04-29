881a02aa8a77dc12f6ee41d2e4e0358e39dbfc0c
Quentin Pongratz <qpongratz@gmail.com>
2022-04-29 17:08:42 -0500

    Add validations to friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,6 @@ class Friendship < ApplicationRecord
   enum :status, %i[pending accepted]
   belongs_to :requester, class_name: 'User'
   belongs_to :friend, class_name: 'User'
+  validates :requester_id, presence: true, uniqueness: { scope: :friend_id }, comparison: { other_than: :friend_id }
+  validates :friend_id, presence: true, uniqueness: { scope: :requester_id }
 end
