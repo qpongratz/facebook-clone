807b3719b14885eb17bde206a78966c291d85d23
Quentin Pongratz <qpongratz@gmail.com>
2022-04-29 09:56:27 -0500

    Update friendship controller tests

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class FriendshipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get friendships_create_url
-    assert_response :success
+  setup do
+    @user = users(:mary)
+    sign_in @user
+    @friend_user = users(:friendless)
   end
 
-  test "should get destroy" do
-    get friendships_destroy_url
-    assert_response :success
+  test "should get create" do
+    assert_difference('Friendship.count') do
+      post "/users/#{@friend_user.id}/friendships"
+    end
   end
+
+  # test "should get destroy" do
+  #   get friendships_destroy_url
+  #   assert_response :success
+  # end
 end
