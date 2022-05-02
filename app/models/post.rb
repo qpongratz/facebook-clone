bc654edb5b607fd287e0c2ac16852718b4c9819e
Quentin Pongratz <qpongratz@gmail.com>
2022-05-02 17:24:24 -0500

    Only show posts by user and friends

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
   validates :content, presence: true
 
   scope :newest_first, -> { order(created_at: :desc) }
+
+  def self.friends_posts(user)
+    friends = user.active_friends
+    where(user: friends).or(where(user: user))
+  end
 end
