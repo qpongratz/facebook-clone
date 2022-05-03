abf734b30855d345582c1dd0d4b08c6607ec44c0
Quentin Pongratz <qpongratz@gmail.com>
2022-05-03 08:31:02 -0500

    Fix formatting on posts tests

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -7,37 +7,37 @@ class PostsTest < ApplicationSystemTestCase
     sign_in @user
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit posts_url
-    assert_selector "h1", text: "Posts"
+    assert_selector 'h1', text: 'Posts'
   end
 
-  test "should create post when signed in" do
+  test 'should create post when signed in' do
     visit posts_url
-     click_on "New post"
+    click_on 'New post'
 
-    fill_in "Content", with: 'Hello'
-    click_on "Create Post"
+    fill_in 'Content', with: 'Hello'
+    click_on 'Create Post'
 
     assert_text 'Hello'
     assert_selector 'h1', text: 'Posts'
   end
 
-  test "should update Post" do
+  test 'should update Post' do
     visit posts_url
-    click_on "Edit this post", match: :first
+    click_on 'Edit this post', match: :first
 
-    fill_in "Content", with: 'Goodbye'
-    click_on "Update Post"
+    fill_in 'Content', with: 'Goodbye'
+    click_on 'Update Post'
 
     assert_text 'Goodbye'
     assert_selector 'h1', text: 'Posts'
   end
 
-  test "should destroy Post" do
+  test 'should destroy Post' do
     visit posts_path
     within "div#post_#{@post.id}" do
-      click_on "Destroy this post", match: :first
+      click_on 'Destroy this post', match: :first
     end
 
     assert_no_selector "div#post_#{@post.id}"
