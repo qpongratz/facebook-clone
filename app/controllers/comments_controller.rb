19bed9eb7237885c9662fbacf9112483964b5869
Quentin Pongratz <qpongratz@gmail.com>
2022-05-03 12:05:27 -0500

    Setup commenting on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -65,6 +65,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:content, :comentable_id, :comentable_type)
+      params.require(:comment).permit(:content, :commentable_id, :commentable_type, :user_id)
     end
 end
