2950c45887ed21fa4e62670ef224045bd970ac9a
Quentin Pongratz <qpongratz@gmail.com>
2022-05-03 11:37:22 -0500

    Add new way to look up friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,15 @@ class Friendship < ApplicationRecord
   validates :requester_id, presence: true, uniqueness: { scope: :friend_id }, comparison: { other_than: :friend_id }
   validates :friend_id, presence: true, uniqueness: { scope: :requester_id }
 
+  # scope :between, -> (user_a_id, user_b_id) {
+  #   find_by(requester_id: user_a_id, friend_id: user_b_id)
+  # }
+
+  def self.between(user_a, user_b)
+    find_by(requester: user_a, friend: user_b) ||
+      find_by(requester: user_b, friend: user_a)
+  end
+
   def member?(user)
     requester == user || friend == user
   end
