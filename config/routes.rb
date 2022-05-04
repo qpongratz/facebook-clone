a1a3a21dd384d8a17dbe70a579a45d980641895d
Quentin Pongratz <qpongratz@gmail.com>
2022-05-04 10:16:01 -0500

    Nest comments under posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+  resources :comments do
+    resources :comments
+  end
   devise_for :users
   resources :users, only: %i[index show] do
     resources :friendships, only: %i[index create update destroy]
