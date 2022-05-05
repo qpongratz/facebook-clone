511bb5c75e6f9c28a1151856c47e650d7cd1b029
Quentin Pongratz <qpongratz@gmail.com>
2022-05-05 10:38:45 -0500

    Remove comments nesting under itslef

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :posts do
-    resources :comments
+    resources :comments, shallow: true, module: :posts
   end
-  resources :comments do
-    resources :comments
-  end
+
   devise_for :users
   resources :users, only: %i[index show] do
     resources :friendships, only: %i[index create update destroy]
