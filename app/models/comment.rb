2a4195b44f6422fb24308cc6905f1a924e896073
Quentin Pongratz <qpongratz@gmail.com>
2022-05-05 10:39:10 -0500

    Change comments relationships to self

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :child_comments, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent_comment, class_name: 'Comment', optional: true
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 end
