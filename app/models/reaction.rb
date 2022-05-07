7f833d6b2ae469e0b5c824da735d7ceeef2def4e
Quentin Pongratz <qpongratz@gmail.com>
2022-05-07 17:45:52 -0500

    Add uniqueness contraint to reactions

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,8 @@
 class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :reactable, polymorphic: true
+
+  validates :user_id, uniqueness: {
+    scope: %i[reactable_type reactable_id],
+    message: 'Only one like per person per thing'}
 end
