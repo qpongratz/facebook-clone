baad21346d5bca0a51c18175131367469d30ef37
Quentin Pongratz <qpongratz@gmail.com>
2022-05-07 20:16:31 -0500

    Solve n+1 for unliking

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.includes(:user, comments: :user)
+    @posts = Post.includes(:user, comments: :user, reactions: :user)
                  .friends_posts(current_user)
                  .newest_first
   end
