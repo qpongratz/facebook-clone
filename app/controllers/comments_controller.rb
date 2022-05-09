74a56ab9aa5c441170cc58fb655240c0c3ef7efe
Quentin Pongratz <qpongratz@gmail.com>
2022-05-09 10:14:39 -0500

    Update routing after comment actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -34,7 +34,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully updated." }
+        format.html { redirect_to @comment, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
         format.turbo_stream
       else
@@ -49,7 +49,7 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.html { redirect_back_or_to root_path, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
       format.turbo_stream
     end
