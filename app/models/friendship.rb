0b674e24cb1927a63295045eaab1ecfe3f8cb129
Quentin Pongratz <qpongratz@gmail.com>
2022-05-11 16:54:05 -0500

    Change friendship for new duties

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,22 +1,20 @@
 class Friendship < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :friend, class_name: 'User'
-  enum :status, %i[pending accepted]
-  belongs_to :requester, class_name: 'User'
+  belongs_to :user
   belongs_to :friend, class_name: 'User'
-  validates :requester_id, presence: true, uniqueness: { scope: :friend_id }, comparison: { other_than: :friend_id }
-  validates :friend_id, presence: true, uniqueness: { scope: :requester_id }
 
-  # scope :between, -> (user_a_id, user_b_id) {
-  #   find_by(requester_id: user_a_id, friend_id: user_b_id)
-  # }
+  validates :user_id, presence: true, uniqueness: { scope: :friend_id }, comparison: { other_than: :friend_id }
+  validates :friend_id, presence: true, uniqueness: { scope: :user_id }
 
-  def self.between(user_a, user_b)
-    find_by(requester: user_a, friend: user_b) ||
-      find_by(requester: user_b, friend: user_a)
+  after_save :create_inverse
+  after_destroy :destroy_inverse
+
+  private
+
+  def destroy_inverse
+    Friendship.destroy_by(user: friend, friend: user)
   end
 
-  def member?(user)
-    requester == user || friend == user
+  def create_inverse
+    Friendship.find_or_create_by(user: friend, friend: user)
   end
 end
