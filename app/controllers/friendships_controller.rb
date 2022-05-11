d708a8b878457cd6f36dd74d74f773111d6b88fb
Quentin Pongratz <qpongratz@gmail.com>
2022-05-11 16:52:45 -0500

    Change friendships controller for new duties

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,38 +1,5 @@
 class FriendshipsController < ApplicationController
-  before_action :set_user
   before_action :set_friendship, only: %i[update destroy]
-
-  def index
-    @pending_incoming = @user.pending_incoming
-    @pending_outgoing = @user.pending_outgoing
-    @active_friends = @user.active_friends
-  end
-
-  def create
-    @friendship = current_user.outgoing_friendships.build(friend_id: @user.id)
-
-    if @friendship.save
-      respond_to do |format|
-        format.html { redirect_back_or_to root_path, notice: 'Friend request sent' }
-        format.turbo_stream
-      end
-    else
-      flash.now[:alert] = 'Something went wrong'
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @friendship.update(status: :accepted)
-      respond_to do |format|
-        format.html { redirect_back_or_to root_path, notice: 'Friend request accepted' }
-        format.turbo_stream
-      end
-    else
-      flash.now[:alert] = 'Something went wrong'
-      render :edit, status: :unprocessable_entity
-    end
-  end
 
   def destroy
     @friendship.destroy
@@ -43,10 +10,6 @@ class FriendshipsController < ApplicationController
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:user_id])
-  end
 
   def set_friendship
     @friendship = Friendship.find(params[:id])
