356c213ea85898cccb0a121b7fefac8919cd12fe
Quentin Pongratz <qpongratz@gmail.com>
2022-05-17 16:58:11 -0500

    Fix new comment rendering on submit

--- app/javascript/controllers/button_submit_controller.js
+++ app/javascript/controllers/button_submit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="button-submit"
 export default class extends Controller {
-  static targets = [ 'button' ]
+  static targets = [ 'button', 'input' ]
 
   submitOnReturn(e){
     if (e.keyCode == 13 && !e.shiftKey){
