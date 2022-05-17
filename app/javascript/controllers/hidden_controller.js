1888af252d3411f9cc657a393655acdfd9a379cb
Quentin Pongratz <qpongratz@gmail.com>
2022-05-17 15:17:06 -0500

    Rework comments display for appended comments

--- app/javascript/controllers/hidden_controller.js
+++ app/javascript/controllers/hidden_controller.js
@@ -2,9 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-hidden"
 export default class extends Controller {
-  static targets = [ 'display' ]
+  static targets = [ 'display', 'clear' ]
 
   toggle() {
-    this.displayTarget.classList.toggle('hidden')
+    this.displayTarget.classList.toggle('hidden');
+    this.removeChildren(this.clearTarget);
+  }
+
+  removeChildren(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
   }
 }
