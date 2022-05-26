80a7c9e1c05656152e3636004689876b9e11066c
Quentin Pongratz <qpongratz@gmail.com>
2022-05-26 09:15:27 -0500

    Update reactions systems test

--- test/system/reactions_test.rb
+++ test/system/reactions_test.rb
@@ -13,16 +13,28 @@ class ReactionsTest < ApplicationSystemTestCase
   test 'should create reaction' do
     visit posts_url
     within "div#post_#{@unliked_post.id}" do
+      within 'div.post_stats_reactions' do
+        assert_no_text '1'
+      end
       click_on 'Like', match: :first
-      assert_text '1 like'
+      within 'div.post_stats_reactions' do
+        assert_text '1'
+      end
     end
   end
 
   test 'should destroy reaction' do
     visit posts_path
     within "div#post_#{@liked_post.id}" do
-      click_on 'Unlike this post', match: :first
-      assert_text '0 likes'
+      within 'div.post_stats_reactions' do
+        assert_text '1'
+      end
+
+      click_on 'Like', match: :first
+
+      within 'div.post_stats_reactions' do
+        assert_no_text '1'
+      end
     end
   end
 end
