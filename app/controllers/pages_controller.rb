af0ce45ff4349d2b13386c73746e7335e7c7f227
Quentin Pongratz <qpongratz@gmail.com>
2022-05-27 15:20:05 -0500

    Add rendering for landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    return unless current_user
-
-    redirect_to posts_path
+    current_user ? (redirect_to posts_path) : (render layout: 'landing_page')
   end
 end
