f8a1fc3459914077bf22ea1c02ed058fab788cf8
Quentin Pongratz <qpongratz@gmail.com>
2022-07-04 12:20:31 -0500

    Remove unneeded code in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,23 +7,5 @@ class Comment < ApplicationRecord
   validates :content, presence: true
   validates :commentable_id, :commentable_type, presence: true
 
-  acts_as_nested_set
-
   scope :ordered, -> { order(id: :asc) }
-
-  def self.preload_tree(actual_depth = nil)
-    # get max_depth
-    max_depth = Category.unscoped.maximum(:depth)
-    actual_depth ||= minimum(:depth)
-
-    return all if max_depth.nil? || actual_depth.nil?
-
-    preloading = :children
-
-    (max_depth - actual_depth).times do
-      preloading = { children: [preloading, :user] } # you can include some other preloads here, if you want, like this: [preloading, :articles]
-    end
-
-    includes(preloading) # or preload, just a matter of taste here
-  end
 end
