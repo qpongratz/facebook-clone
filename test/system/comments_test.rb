1aa51a878b8de2cee8ace47bd6ffde331c95d2f2
Quentin Pongratz <qpongratz@gmail.com>
2022-07-04 14:02:37 -0500

    Reimplement comment system tests

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -1,45 +1,48 @@
-# require "application_system_test_case"
+require "application_system_test_case"
 
-# class CommentsTest < ApplicationSystemTestCase
-#   setup do
-#     @post = posts(:one)
-#     @user = users(:mary)
-#     @comment = comments(:one)
-#     sign_in @user
-#   end
+class CommentsTest < ApplicationSystemTestCase
+  setup do
+    @post = posts(:one)
+    @user = users(:mary)
+    @comment = comments(:one)
+    sign_in @user
+    Post.reset_counters(@post.id, :comments)
+  end
 
-#   test 'should create comment' do
-#     visit posts_url
-#     within "div#post_#{@post.id}" do
-#       click_on 'Add a comment', match: :first
-#     end
+  test 'should create comment' do
+    visit posts_url
+    within "div#post_#{@post.id}" do
+      fill_in 'Content', with: 'Hello'
+      find('button[aria-label=submit]').click
+    end
+    assert_text 'Hello'
+  end
 
-#     fill_in 'Content', with: 'Hello'
-#     click_on 'Create Comment'
+  test 'should update comment' do
+    visit posts_url
+    within "div#post_#{@post.id}" do
+      click_on '1 comment', match: :first
+      within "div#comment_#{@comment.id}" do
+        find('button[aria-label=options]', visible: false).click
+        click_on 'Edit', match: :first
+        fill_in 'Content', with: 'Goodbye'
+        find('button[aria-label=submit]').click
+      end
 
-#     assert_text 'Hello'
-#     assert_selector 'h1', text: 'Posts'
-#   end
+      assert_text 'Goodbye'
+    end
+  end
 
-#   test 'should update comment' do
-#     visit posts_url
-#     within "li#comment_#{@comment.id}" do
-#       click_on 'Edit this comment', match: :first
-#     end
+  test 'should delete comment' do
+    visit posts_url
+    within "div#post_#{@post.id}" do
+      click_on '1 comment', match: :first
+      within "div#comment_#{@comment.id}" do
+        find('button[aria-label=options]', visible: false).click
+        click_on 'Delete', match: :first
+      end
 
-#     fill_in 'Content', with: 'Goodbye'
-#     click_on 'Update Comment'
-
-#     assert_text 'Goodbye'
-#     assert_selector 'h1', text: 'Posts'
-#   end
-
-#   test 'should destroy comment' do
-#     visit posts_path
-#     within "li#comment_#{@comment.id}" do
-#       click_on 'Destroy this comment', match: :first
-#     end
-
-#     assert_no_selector "li#comment_#{@comment.id}"
-#   end
-# end
+      assert_no_selector "div#comment_#{@comment.id}"
+    end
+  end
+end
