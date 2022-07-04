b9e2a3941d80ecd96a0e144d8abff33f336d1401
Quentin Pongratz <qpongratz@gmail.com>
2022-07-04 12:50:47 -0500

    Add test for friends_posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,10 +3,19 @@ require "test_helper"
 class PostTest < ActiveSupport::TestCase
   def setup
     @new_post = posts(:newer)
+    @mary = users(:mary)
+    @joe_post = posts(:joe_post)
+    @friendless_post = posts(:friendless_post)
   end
 
   test '#newest_first' do
     assert_equal Post.all.newest_first.first, @new_post
+  end
+
+  test '#friends_posts' do
+    friends_posts = Post.friends_posts(@mary)
+    assert_includes friends_posts, @joe_post
+    refute_includes friends_posts, @friendless_post
   end
 
 end
